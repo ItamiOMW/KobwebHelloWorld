f9a36a2ca508738a7eead6f63ab2b4c3a39f67ab
Вадим <itamiomw@gmail.com>
2023-12-30 15:45:56 +0300

    Changed home page text to "Hello World!"

--- site/src/jsMain/kotlin/com/itami/kobweb_hello_world/pages/Index.kt
+++ site/src/jsMain/kotlin/com/itami/kobweb_hello_world/pages/Index.kt
@@ -1,18 +1,23 @@
 package com.itami.kobweb_hello_world.pages
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        H1 {
+            Text(value = "Hello World!")
+        }
     }
 }
